3735ff55ba250dbede0f8d47203dbc62176981c7
oleeriksen <oleeriksen1958@gmail.com>
2024-01-31 12:57:51 +0100

    Update App.cs

--- Calculator/App.cs
+++ Calculator/App.cs
@@ -4,7 +4,7 @@ public class App
 {
     public void Run() {
         while (true) {
-            int a = int.Parse(Console.ReadLine());
+            int a = ReadInt("Indtast første tal:");
             var ch = Console.ReadKey().KeyChar;
             if (ch != '+')
             {
@@ -12,11 +12,13 @@ public class App
                 continue;
             }
             Console.WriteLine();
-            int b = int.Parse(Console.ReadLine());
+            int b = ReadInt("Indtast andet tal:");
             Console.WriteLine($"Resultat = {a+b}");
         }
+    }
 
-
-
+    int ReadInt(string leadingText) {
+        Console.Write(leadingText);
+        return Convert.ToInt32(Console.ReadLine());
     }
 }
