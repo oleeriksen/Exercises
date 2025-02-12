eea416dd7c6c024a49983a0d893685857376f7bd
oleeriksen <oe@easv.dk>
2025-02-12 11:56:15 +0100

    Initial commit

---
+++ WordguesserConsole/RandomWords.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace WordguesserConsole
+{
+    class RandomWords
+    {
+
+
+        string[] m_words;
+        Random m_random;
+
+        public RandomWords()
+        {
+            m_random = new Random();
+            m_words = new string[] { "Skanderborg",
+                                      "Silkeborg",
+                                      "Grenå",
+                                      "Horsens",
+                                      "Beder",
+                                      "Odder",
+                                      "Sabro",
+                                      "Harlev",
+                                      "Trige",
+                                      "Hadsten",
+                                      "Hammel",
+                                      "Malling"};
+        }
+
+        public string GetNext()
+        {
+            int idx = m_random.Next(m_words.Length);
+            return m_words[idx];
+        }
+
+        public string GetHint() {
+            return "Byer i omegnen af Aarhus";
+        }
+
+    }
+}
+
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Exercises
+
+Dette repo bruges til at udgive/dele kode for større opgaver.
+
+WordGuesser - et konsol program og spil som bruges tl at gætte ord.
